15ccd5c95d9f003ddacb1e604e45c96b3fd57c75
Ro <filpoyma@gmail.com>
2018-06-21 12:25:49 +0300

    app_med

---
+++ src/Medic.java
@@ -0,0 +1,15 @@
+
+
+public class Medic {
+
+    //common
+    //static - означает, что поле класса (переменная) имеет одно значение для всех реализаций класса
+    public static int score = 100;
+    public static int health = 10;
+
+    //personal
+    // non-static (dinamic) - поле класса имеет отдельное значение для каждой реализации класса
+    public String name = "Ivan";
+    public int health_personal = 100;
+}
+
