6f93fbdb77d6e04444747dc1005973e4c40dc7ee
Ro <filpoyma@gmail.com>
2018-06-21 12:32:55 +0300

    app_med

--- src/App.java
+++ src/App.java
@@ -11,11 +11,11 @@ public class App {
         medic3.health--; // Static field
         System.out.println("Static fields:");
         System.out.printf( "Medic-1 -> %d\n", medic1.health );
-        System.out.printf( "Medic-2 -> %d\n", medic2.health ); // результат будет общий, тк hp - static
+        System.out.printf( "Medic-2 -> %d\n", medic2.health ); // СЂРµР·СѓР»СЊС‚Р°С‚ Р±СѓРґРµС‚ РѕР±С‰РёР№, С‚Рє hp - static
         System.out.printf( "Medic-3 -> %d\n", medic3.health );
 
-        // в реале правильнее обращаться статическим переменным таким образом: Medic.health
-        //т.к. переменная health она одна для всех реализаций класса Medic
+        // РІ СЂРµР°Р»Рµ РїСЂР°РІРёР»СЊРЅРµРµ РѕР±СЂР°С‰Р°С‚СЊСЃСЏ СЃС‚Р°С‚РёС‡РµСЃРєРёРј РїРµСЂРµРјРµРЅРЅС‹Рј С‚Р°РєРёРј РѕР±СЂР°Р·РѕРј: Medic.health
+        //С‚.Рє. РїРµСЂРµРјРµРЅРЅР°СЏ health РѕРЅР° РѕРґРЅР° РґР»СЏ РІСЃРµС… СЂРµР°Р»РёР·Р°С†РёР№ РєР»Р°СЃСЃР° Medic
         System.out.printf("Health for all Medics - %d\n\n", Medic.health);
 
         System.out.println("Non static & Static fields:");
@@ -23,8 +23,8 @@ public class App {
         Medic med_plr2 = new Medic();
         Medic med_plr3 = new Medic();
 
-        //med_player1.score -= 8; // Static field можно и так записать, но...
-        Medic.score -= 8; // - правильная запись для Ststic полей
+        //med_player1.score -= 8; // Static field РјРѕР¶РЅРѕ Рё С‚Р°Рє Р·Р°РїРёСЃР°С‚СЊ, РЅРѕ...
+        Medic.score -= 8; // - РїСЂР°РІРёР»СЊРЅР°СЏ Р·Р°РїРёСЃСЊ РґР»СЏ Ststic РїРѕР»РµР№
         med_plr1.health_personal -= 40; // Non Static field
         med_plr2.health_personal = 10; // Non Static field
         med_plr3.health_personal = 30; // Non Static field
